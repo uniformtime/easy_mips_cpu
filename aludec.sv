d855f889d4bc3d04a1dd51dfe04992a3a8203deb
uniformtime <uniformtime@mail.ru>
2022-03-08 01:49:23 +0500

    added: adder.sv, datapath.sv, flopr.sv, regfile.sv, sidnext.sv, sl2.sv changed: aludec.sv

--- aludec.sv
+++ aludec.sv
@@ -8,5 +8,12 @@ always_comb
 	2'b00: alucontrol <= 3'b010; // add (for lw/sw/addi)
 	2'b01: alucontrol <= 3'b110; // sub (for beq)
 	default: case(funct)	     // R-type instructions
-	    6'b100000: alucontrol <=3'b010; // add
-	    6'b100010: 
+	    6'b100000: alucontrol <= 3'b010; // add
+	    6'b100010: alucontrol <= 3'b110; // sub
+	    6'1001000: alucontrol <= 3'b000; // and
+	    6'b100101: alucontrol <= 3'b001; // or
+	    6'b101010: alucontrol <= 3'b111; // slt
+	    default:   alucontrol <= 3'bxxx; // ???
+	endcase
+    endcase
+endmodule
